dfc465cdea4083ffb35dd604972fc45bb7d966a5
Ross Teixeira <rapt@cs.princeton.edu>
2018-02-05 14:18:17 -0500

    uploaded config files

---
+++ assignments/config_files/jupyter_notebook_config.py
@@ -0,0 +1,11 @@
+## Configuration file for jupyter-notebook.
+## This file will be copied to ~/.jupyter/jupyter_notebook_config.py in the VM
+## and is essential for the jupyter notebook to function correctly,
+## please do not modify!
+
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.notebook_dir = '/vagrant'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
+c.NotebookApp.token = ''
+c.NotebookApp.allow_root = True
